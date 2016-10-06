ad133d854b0a0468d39fb729ba9015fdac45a5b3
Zhengyu Yan <a06@julyinc.com>
2016-10-06 15:24:33 +0900

    Auth実装

--- app/Model/User.php
+++ app/Model/User.php
@@ -18,6 +18,16 @@ class User extends AppModel {
  */
 	public $displayField = 'username';
 
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new BlowfishPasswordHasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash(
+                $this->data[$this->alias]['password']
+            );
+        }
+        return true;
+    }
+
 /**
  * Validation rules
  *
